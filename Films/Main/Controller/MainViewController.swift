778ce2c609db920fdfd1f5c6e6aee902a723d2a0
DarthKreo <66432033+DarthKreo@users.noreply.github.com>
2020-10-06 16:38:25 +0300

    start

---
+++ Films/Main/Controller/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Films
+//
+//  Created by Владимир Кваша on 06.10.2020.
+//
+
+import UIKit
+
+
+// MARK: - MainViewController
+
+final class MainViewController: UIViewController {
+
+    //MARK: - Private properties
+    
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+//MARK: - Actions
+    
+}
+
+// MARK: - Setup
+
+// MARK: - Setups Views
+
+// MARK: - Layout
+
+// MARK: - Constants
